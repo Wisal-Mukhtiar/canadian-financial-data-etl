42e4e6d57fa3055fc50221056ab5bafdf60d64b6
Wisal <wisalmukhtiar7@gmail.com>
2023-04-30 16:10:31 +0500

    changing code to use function and adding data processing

--- ETL/etl.py
+++ ETL/etl.py
@@ -10,26 +10,71 @@ import decimal
 
 
 class MyException(Exception):
+    """A custom exception class for raising custom errors"""
+
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
 
-# initiate configuratin files parser
-config = configparser.ConfigParser()
-file_read = config.read('project.ini')
-if len(file_read) == 0:
-    raise MyException("Couldn't read file")
+def read_configuration(file_name):
+    """reads configuration file
 
-# values from configuration file
-start_date = config['CONFIG']['startDate']
-url = config['CONFIG']['url']
-dest_server = config['CONFIG']['server']
-dest_database = config['CONFIG']['database']
+        return configuration tuple:
+            (start_date, url, dest_server, dest_database)
+    """
+    config = configparser.ConfigParser()
+
+    file_read = config.read(file_name)
+    if len(file_read) == 0:
+        raise MyException("Couldn't read file")
+
+    # values from configuration file
+    start_date = config['CONFIG']['startDate']
+    url = config['CONFIG']['url']
+    dest_server = config['CONFIG']['server']
+    dest_db = config['CONFIG']['database']
+
+    return start_date, url, dest_server, dest_db
 
 
-# requesting data from Bank Of Canada's API
-try:
-    boc_resp = requests.get(url+start_date)
-except Exception as e:
-    raise MyException("Couldn't read data from url : ", e)
+def fetch_data(url, start_date):
+    """Takes the url of the bank api and start date
+
+      returns raw data from the api response as a dictionary/Json
+    """
+    try:
+        boc_resp = requests.get(url+start_date)
+    except Exception as e:
+        raise MyException("Couldn't read data from url : ", e)
+
+    if (boc_resp.status_code == 200):
+        boc_raw = json.loads(boc_resp.text)
+
+    return boc_raw
+
+
+def process_data(raw_data):
+    """
+    recieves raw data dictionary
+    return a list of lists containing
+    rate_list and correspoding date list
+
+    """
+    rate = []
+    date = []
+    for row in raw_data['observations']:
+        date.append(datetime.datetime.strptime(row['d'], '%Y-%m-%d'))
+        rate.append(decimal.Decimal(row['FXUSDCAD']['v']))
+
+    return [rate, date]
+
+
+def main():
+    start_date, url, dest_server, dest_db = read_configuration('project.ini')
+    raw_data = fetch_data(url, start_date)
+    rate_date = process_data(raw_data)
+
+
+if __name__ == '__main__':
+    main()
